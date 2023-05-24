396aa4e1ff5340c6aaf83a3e854fe19e3c1cd0fe
Christoffer Jahren <christoffer.jahren@schibsted.com>
2023-05-24 10:32:41 +0200

    fix: attempt at including types asset (even though gitignored) with semantic release

--- release.config.cjs
+++ release.config.cjs
@@ -2,7 +2,17 @@ module.exports = {
   plugins: [
     "@semantic-release/changelog",
     "@semantic-release/npm",
-    "@semantic-release/git",
+    [
+      "@semantic-release/git",
+      {
+        assets: [
+          "src/index.js",
+          "types/index.d.ts",
+          "package.json",
+          "README.md",
+        ],
+      },
+    ],
   ],
   preset: "angular",
   branches: [{ name: "main" }, { name: "next", prerelease: true }],
